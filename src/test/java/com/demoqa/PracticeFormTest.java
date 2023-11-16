b664f199956609b700aac99655cce747b6fb22ad
AndreiMauchun <andreymovchun@gmail.com>
2023-11-16 18:33:09 +0300

    added test

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -1,2 +1,38 @@
-package com.demoqa;public class PracticeFormTest {
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTest {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        $("#firstName").setValue("Andrey");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("Ivanov@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("+375295893256");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1991");
+        $(".react-datepicker__day--017:not(.react-datepicker__day--selected").click();
+        $("#subjectsInput").setValue("English").pressTab();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+
+    }
 }
