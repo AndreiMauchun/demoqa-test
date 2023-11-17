0d97c71c4ec06bf652d8eac9515c444085576b67
AndreiMauchun <andreymovchun@gmail.com>
2023-11-17 11:07:58 +0300

    Updated test

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -14,7 +14,7 @@ public class PracticeFormTest {
     @BeforeAll
     static void configure() {
         Configuration.baseUrl = "https://demoqa.com";
-
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -26,13 +26,35 @@ public class PracticeFormTest {
         $("#lastName").setValue("Ivanov");
         $("#userEmail").setValue("Ivanov@gmail.com");
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("+375295893256");
+        $("#userNumber").setValue("375295893256");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("September");
         $(".react-datepicker__year-select").selectOption("1991");
         $(".react-datepicker__day--017:not(.react-datepicker__day--selected").click();
         $("#subjectsInput").setValue("English").pressTab();
         $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("orlovskogo 20");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive").shouldHave(text("Andrey Ivanov"),
+
+                text("Ivanov@gmail.com"),
+                text("Male"),
+                text("3752958932"),
+                text("17 September,1991"),
+                text("English"),
+                text("Sports"),
+                text("1.png"),
+                text("orlovskogo 20"),
+                text("NCR Delhi"));
 
     }
 }
